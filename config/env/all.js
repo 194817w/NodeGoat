92b79843d17564a8f8572f2455c95c76c507ff49
Nurul Aisha <194817w@mymail.nyp.edu.sg>
2021-11-03 03:22:26 +0000

    Update all.js

--- config/env/all.js
+++ config/env/all.js
@@ -1,6 +1,6 @@
 // default app configuration
 const port = process.env.PORT || 4000;
-let db = process.env.MONGODB_URI || "mongodb://localhost:27017/nodegoat";
+let db = process.env.MONGODB_URI || "mongodb://mongo:27017/nodegoat";
 
 module.exports = {
     port,
@@ -8,7 +8,7 @@ module.exports = {
     cookieSecret: "session_cookie_secret_key_here",
     cryptoKey: "a_secure_key_for_crypto_here",
     cryptoAlgo: "aes256",
-    hostName: "localhost",
+    hostName: "nodegoat",
     environmentalScripts: []
 };
 
