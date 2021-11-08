bb51ac17a3a3b6e7dd38cfae5b58d7a371bb5d7e
Nurul Aisha <194817w@mymail.nyp.edu.sg>
2021-11-08 03:27:46 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+RUN export CGO_ENABLED=0 && go build
+
 until nc -z -w 2 mongo 27017 && echo 'mongo is ready for connections' && node artifacts/db-reset.js && npm start; do sleep 2; done
